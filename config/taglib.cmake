db9c6b7ae19892f5d39e75f28c9a1f24643eec9c
XiNiHa <anesin1109@naver.com>
2019-09-26 15:10:48 -0400

    Fix Windows Build

--- config/taglib.cmake
+++ config/taglib.cmake
@@ -1,15 +1,11 @@
 function(AddTaglibSupport addonName)
-  if(MSVC)
-    set(WIN_OPTIONS "-DBUILD_SHARED_LIBS=OFF -DENABLE_STATIC_RUNTIME=ON")
-  endif()
-
   ExternalProject_Add(
     taglib
     PREFIX "${CMAKE_SOURCE_DIR}/taglib"
     GIT_REPOSITORY https://github.com/taglib/taglib
     GIT_TAG ${TAGLIB_VERSION}
     INSTALL_DIR "${CMAKE_SOURCE_DIR}/taglib"
-    CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/taglib" "${WIN_OPTIONS}"
+    CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/taglib"
     CMAKE_CACHE_ARGS "-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true"
   )
 
@@ -25,10 +21,10 @@ function(AddTaglibSupport addonName)
   target_include_directories(${addonName} PRIVATE ${CMAKE_SOURCE_DIR}/taglib/include)
 
   if (WIN32)
-    target_link_libraries(${addonName} PRIVATE ${CMAKE_JS_LIB} ${CMAKE_SOURCE_DIR}/taglib/lib/tag.lib)
+    target_link_libraries(${addonName} ${CMAKE_SOURCE_DIR}/taglib/lib/tag.lib)
+  elseif (UNIX AND NOT APPLE)
+    target_link_libraries(${addonName} ${CMAKE_SOURCE_DIR}/taglib/lib/libtag.a)
   endif()
 
-  if (UNIX AND NOT APPLE)
-    target_link_libraries(${addonName} PRIVATE ${CMAKE_JS_LIB} ${CMAKE_SOURCE_DIR}/taglib/lib/libtag.a)
-  endif()
+  add_definitions(-DTAGLIB_STATIC)
 endfunction(AddTaglibSupport addonName)
